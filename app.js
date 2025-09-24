9b93a8128a84d0bdca6ce063748cbf993e9f1436
baljaa <baljinnyam9088@gmail.com>
2025-09-24 22:19:08 +0200

    event listeneruudiig uusgej ugluu

---
+++ app.js
@@ -0,0 +1,22 @@
+var uiController=(function(){
+    
+})();
+
+
+var financeController=(function(){
+    
+})();
+
+var appController=(function(uiController,fnController){
+    var ctrlAddItem=function(){
+        // console.log("gg");
+    };
+    document.querySelector('.add__btn').addEventListener('click',function(){
+        ctrlAddItem();
+    });
+    document.addEventListener("keypress",function(event){
+        if(event.keyCode ===13|| event.which===13){
+            ctrlAddItem();
+        }
+    });
+})(uiController,financeController);
