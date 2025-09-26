37b488b2435224b2aeedfea658c8fe4721f91e45
baljaa <baljinnyam9088@gmail.com>
2025-09-26 16:12:41 +0200

    finance module hiij ehelsen. expense income objectuudiig uusgene

--- app.js
+++ app.js
@@ -1,22 +1,77 @@
 var uiController=(function(){
-    
+    var DOMstrings={
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        addBtn: ".add__btn"
+    };
+    return{
+        getInput: function(){
+            return{
+                type: document.querySelector(DOMstrings.inputType).value,
+                Description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
 })();
 
 
 var financeController=(function(){
-    
+    var Income = function(id,description,value){
+        this.id=id;
+        this.description=description;
+        this.value=value;
+    };
+
+    var Expense = function(id,description,value){
+        this.id=id;
+        this.description=description;
+        this.value=value;
+    }
+    var incomes=[];
+    var expenses=[];
+    var totalIncomes=0;
+    var totalExpenses=0;
+
+    var data={
+        allItems:{
+            inc:[],
+            exp:[]
+        },
+        totals:{
+            inc:0,
+            exp:0
+        }
+    }
 })();
 
 var appController=(function(uiController,fnController){
+
     var ctrlAddItem=function(){
         // console.log("gg");
     };
-    document.querySelector('.add__btn').addEventListener('click',function(){
+    
+    var setupEventListeners= function(){
+        var DOM=uiController.getDOMstrings();
+        document.querySelector('.add__btn').addEventListener('click',function(){
         ctrlAddItem();
-    });
-    document.addEventListener("keypress",function(event){
-        if(event.keyCode ===13|| event.which===13){
-            ctrlAddItem();
+        });
+
+        document.addEventListener("keypress",function(event){
+            if(event.keyCode ===13|| event.which===13){
+                ctrlAddItem();
+            }
+        });
+        };
+    return{
+        init: function(){
+            setupEventListeners();
         }
-    });
+    }
 })(uiController,financeController);
+
+appController.init();
